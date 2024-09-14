a56d854b3682c833001b7112d52828ff3a41f82e
ebisyenah <ebrahim.soroush@syenah.ai>
2024-09-14 13:53:18 +0200

    Adds new cool plugins: * code snapshot * neogit * Trouble

--- lua/plugins/neogit.lua
+++ lua/plugins/neogit.lua
@@ -1,3 +1,14 @@
+vim.keymap.set('n', '<leader>gs', vim.cmd.Neogit, { silent = true, noremap = true, desc = 'open neogit window' })
+
+vim.keymap.set('n', '<leader>gc', ':Neogit commit<CR>', { silent = true, noremap = true, desc = 'Commit using neogit' })
+
+vim.keymap.set('n', '<leader>gp', ':Neogit pull<CR>', { silent = true, noremap = true, desc = 'pull using neogit' })
+
+vim.keymap.set('n', '<leader>gP', ':Neogit push<CR>', { silent = true, noremap = true, desc = 'push using neogit' })
+
+vim.keymap.set('n', '<leader>gb', ':Telescope git_branches<CR>', { silent = true, noremap = true, desc = 'neogit branches using telescope' })
+
+vim.keymap.set('n', '<leader>gB', ':G blame<CR>', { silent = true, noremap = true, desc = 'neogi blame' })
 return {
   'NeogitOrg/neogit',
   lazy = false,
