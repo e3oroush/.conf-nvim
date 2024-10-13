ed89c62da2bbee16d5e3c0b186ff8d9e623ab804
e3oroush <ebrahim.soroush@gmail.com>
2024-10-13 19:30:32 +0200

    adds some global utils for plugin dev

--- lua/core/utils.lua
+++ lua/core/utils.lua
@@ -1,3 +1,13 @@
-vim.cmd [[
-  command! FormatJSON %!jq .
-]]
+P = function(v)
+  print(vim.inspect(v))
+  return v
+end
+
+RELOAD = function(...)
+  return require('plenary.reload').reload_module(...)
+end
+
+R = function(name)
+  RELOAD(name)
+  return require(name)
+end
